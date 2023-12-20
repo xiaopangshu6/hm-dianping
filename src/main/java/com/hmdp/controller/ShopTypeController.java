4fd1340002e139ab5db1f81f7f97750299ea9a14
xiaopangshu6 <153588978+xiaopangshu6@users.noreply.github.com>
2023-12-20 17:44:07 +0800

    第一次提交

---
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -0,0 +1,48 @@
+package com.hmdp.controller;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IShopTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@RestController
+@RequestMapping("/shop-type")
+public class ShopTypeController {
+    @Resource
+    private IShopTypeService typeService;
+
+    @GetMapping("list")
+    public Result queryTypeList() {
+        List<ShopType> typeList = typeService
+                .query().orderByAsc("sort").list();
+        return Result.ok(typeList);
+    }
+}
+
+
+
